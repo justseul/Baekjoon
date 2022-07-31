2abdb6afaaa5d521b85d668670ca22f4aea9ad93
Kim Seul <108224701+justseul@users.noreply.github.com>
2022-07-31 22:41:02 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -0,0 +1,12 @@
+Croatia = input()
+
+Croatia =Croatia.replace('c=','c')
+Croatia =Croatia.replace('c-','c')
+Croatia =Croatia.replace('dz=','c')
+Croatia =Croatia.replace('d-','c')
+Croatia =Croatia.replace('lj','c')
+Croatia =Croatia.replace('nj','c')
+Croatia =Croatia.replace('s=','c')
+Croatia =Croatia.replace('z=','c')
+
+print(len(Croatia))
