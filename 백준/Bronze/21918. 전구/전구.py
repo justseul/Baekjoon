ba1088f1c86535dc88949791178b8fb50fa26e6c
Kim Seul <108224701+justseul@users.noreply.github.com>
2022-08-03 01:41:13 +0900

    [Bronze II] Title: 전구, Time: 212 ms, Memory: 117208 KB -BaekjoonHub

---
+++ 백준/Bronze/21918. 전구/전구.py
@@ -0,0 +1,23 @@
+N, M = map(int,input().split())
+
+s = list(map(int, input().split()))
+    
+for _ in range(M):    
+    a, b, c = map(int,input().split())
+    
+    if(a == 1):
+        s[b-1] = c
+    elif(a == 2):
+        for i in range(b-1,c):
+            if(s[i]==1):
+                s[i] = 0
+            else:
+                s[i] = 1
+    elif(a == 3):
+        for i in range(b-1,c):
+            s[i] = 0
+    else:
+        for i in range(b-1,c):
+            s[i] = 1
+print(' '.join(map(str,s)))
+  
