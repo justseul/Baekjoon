83ff5bda2c68e4c2f5c86322131b1990b90736cb
Kim Seul <108224701+justseul@users.noreply.github.com>
2022-10-14 23:10:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 47.05 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,22 @@
+def solution(participant, completion):
+    dic = {}
+
+    for i in participant:
+        if i in dic:
+            dic[i] += 1
+        else:
+            dic[i] = 1
+            
+    for i in completion:
+        if i in dic:
+            dic[i] -= 1
+            
+    for i in dic:
+        if dic[i] > 0:
+            return  i
+
+    
+    
+
+
+
