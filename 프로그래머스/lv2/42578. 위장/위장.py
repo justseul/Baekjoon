250bd21a07d2221f6498a797be5a31d41b9bf016
Kim Seul <108224701+justseul@users.noreply.github.com>
2022-10-18 08:13:34 +0900

    [level 2] Title: 위장, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.py
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -3,14 +3,14 @@ def solution(clothes):
     m = 1
 
     for i, j in clothes:
-        dic[j] = 0
+        dic[j] = 1
 
     for i, j in clothes:
         if j in dic.keys():
             dic[j] += 1
 
     for i in dic.values():
-        m *= i +1
+        m *= i 
 
     answer = m -1
     return answer
