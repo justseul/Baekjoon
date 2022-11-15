fa27f331dbb982610b1ade9b3e3c9cb7190d7ff2
Kim Seul <108224701+justseul@users.noreply.github.com>
2022-11-16 04:01:47 +0900

    [level 2] Title: 피로도, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,21 +1,3 @@
-from itertools import permutations
-
 def solution(k, dungeons):
-    sample = list(permutations(dungeons, len(dungeons)))
-    sample_cnt = []
-
-    for i in range(len(sample)):
-        cnt = 0
-        k_1 = k
-        for j in range(len(dungeons)):
-            if(k_1 < sample[i][j][0] ):
-                pass
-            else:
-                k_1 -= sample[i][j][1]
-                cnt += 1
-
-
-        sample_cnt.append(cnt)
-
-    
-    return max(sample_cnt)
+    answer = -1
+    return answer
