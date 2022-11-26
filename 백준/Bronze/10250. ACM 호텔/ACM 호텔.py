b3f7959fbbb6d6c4c4bbee8e612ee95069597aa5
Kim Seul <108224701+justseul@users.noreply.github.com>
2022-11-26 13:10:29 +0900

    [Bronze III] Title: ACM 호텔, Time: 120 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,13 @@
+n = int(input())
+for i in range(n):
+    H, W, N = map(int,input().split())
+    if(N % H == 0):
+        if(N // H +1 <= 10 ):
+            print(str(H)+"0"+str(N // H))
+        else:
+            print(str(H)+str(N // H))
+    else:
+        if(N // H +1 < 10):
+            print(str(N % H)+"0"+str(N // H +1))
+        else:
+            print(str(N % H)+str(N // H +1))
