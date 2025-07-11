1cccac3cfe2b36cdbc31124c3376b4fad887b9bf
Kim Seul <108224701+justseul@users.noreply.github.com>
2025-07-11 14:10:52 +0900

    [Silver I] Title: 단지번호붙이기, Time: 96 ms, Memory: 109704 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,35 +1,32 @@
-def bfs(x, y):
-    b_list = [(x, y)]
-    maps[x][y] = 0
-    cnt = 1
-    while b_list:
-        x, y = b_list.pop(0)
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
-                continue
-            if maps[nx][ny] == 0:
-                continue
-            if maps[nx][ny] == 1:
-                cnt += 1
-                b_list.append((nx,ny))
-                maps[nx][ny] = 0
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(map(int, input().strip())) for i in range(n)]
+
+all_cnt = 0
+answer = list()
+cnt = 0
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+def DFS(x, y):
+    global cnt
+    cnt += 1
+    graph[x][y] = 0
+    for d in range(4):
+        nx = x + dx[d]
+        ny = y + dy[d]
+        if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:
+            DFS(nx, ny)
     return cnt
 
-        
-complex = []
-maps = []  # 변수 이름 변경
-n = int(input())
-for i in range(n):
-    maps.append(list(map(int, input())))    
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 for i in range(n):
     for j in range(n):
-        if maps[i][j] == 1:
-            complex.append(bfs(i,j))
-print(len(complex))
-for i in sorted(complex):
-    print(i)
+        if graph[i][j] == 1:
+            cnt = 0
+            answer.append(DFS(i, j))
+
+answer.sort()
+print(len(answer))
+print(*answer, sep="\n")
